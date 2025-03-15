b9ff9c60ce387fb5186232e9c53c93578c766b92
kapolos <nospamplease@kapolos.com>
2025-03-15 18:18:12 +0200

    * separated impl for non-clone * refactored proc macro outside `derive` * added changelog * updated docs

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# 0.10.0 (breaking changes)
+
+* separate implementation for non-clone
+* proc macro is now independent of derive
+
+## 0.9.0
+
+* Initial release
--- src/lib.rs
+++ src/lib.rs
@@ -1,149 +1,80 @@
 extern crate proc_macro;
 use proc_macro::TokenStream;
 use convert_case::{Case, Casing};
-use proc_macro2::TokenStream as TokenStream2;
 use quote::{format_ident, quote};
 use syn::{parse_macro_input, DeriveInput};
 use syn::{Data, DataStruct, Fields};
+use syn::parse::{Parse, ParseStream, Result};
+use syn::{Ident, Token};
+use syn::spanned::Spanned;
 
-/// Automatically implements the `Field` trait for each named field of a struct.
-///
-/// For a struct like:
-///
-/// ```rust
-/// struct MyStruct {
-///     foo: i32,
-///     bar: String,
-/// }
-/// ```
-///
-/// This macro generates unit structs (e.g. `FooField`, `BarField`) and implements a
-/// `Field` trait for each, enabling type-safe access and update of individual fields:
-///
-/// ```rust
-/// pub struct FooField;
-/// impl Field for FooField {
-///     type FieldType = i32;
-///     fn select<'a>(&self, store: &'a MyStruct) -> &'a Self::FieldType {
-///         &store.foo
-///     }
-///     fn set(&self, store: &mut MyStruct, value: Self::FieldType) {
-///         store.foo = value;
-///     }
-/// }
-///
-/// pub struct BarField;
-/// impl Field for BarField {
-///     type FieldType = String;
-///     fn select<'a>(&self, store: &'a MyStruct) -> &'a Self::FieldType {
-///         &store.bar
-///     }
-///     fn set(&self, store: &mut MyStruct, value: Self::FieldType) {
-///         store.bar = value;
-///     }
-/// }
-/// ```
-///
-/// This macro also generates the definition of the `Field` trait.
-#[proc_macro_derive(FieldEnum)]
-pub fn field_enum_derive(input: TokenStream) -> TokenStream {
-    // Parse the input tokens into a syntax tree
-    let input = parse_macro_input!(input as DeriveInput);
+/// Parser for attribute arguments.
+struct CreteArgs {
+    clone: bool
+}
 
-    let name = &input.ident;
+impl Parse for CreteArgs {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let mut clone = false;
 
-    // Get the fields of the struct
-    let fields = if let Data::Struct(DataStruct {
-        fields: Fields::Named(ref fields_named),
-        ..
-    }) = input.data
-    {
-        &fields_named.named
-    } else {
-        panic!("FieldEnum can only be used with named fields");
-    };
+        // If the input is empty, no attributes were provided.
+        if input.is_empty() {
+            return Ok(CreteArgs { clone: false });
+        }
 
-    // Generate unit structs and trait implementations
-    let unit_structs = fields.iter().map(|field| {
-        let ident = field.ident.as_ref().unwrap();
-        let struct_name = format_ident!("{}Field", ident.to_string().to_case(Case::Pascal));
-        let ty = &field.ty;
-        quote! {
-            pub struct #struct_name;
+        // Parse comma-separated identifiers.
+        while !input.is_empty() {
+            let ident: Ident = input.parse()?;
 
-            impl Field for #struct_name {
-                type FieldType = #ty;
-                fn select<'a>(&self, store: &'a #name) -> &'a Self::FieldType {
-                    &store.#ident
+            if ident == "Clone" {
+                if clone {
+                    // Error if "Clone" is specified more than once.
+                    return Err(syn::Error::new(
+                        ident.span(),
+                        "Duplicate 'Clone' attribute",
+                    ));
                 }
-                fn set(&self, store: &mut #name, value: Self::FieldType) {
-                    store.#ident = value;
+                clone = true;
+            } else {
+                return Err(syn::Error::new(
+                    ident.span(),
+                    format!("Unexpected attribute '{}'. Expected 'Clone'.", ident),
+                ));
+            }
+
+            // If there's more input, expect a comma.
+            if !input.is_empty() {
+                let comma: Token![,] = input.parse()?;
+                if input.is_empty() {
+                    return Err(syn::Error::new(
+                        comma.span(),
+                        "Trailing comma not allowed",
+                    ));
                 }
             }
         }
-    });
 
-    // Generate the Field trait
-    let expanded = quote! {
-        pub trait Field {
-            type FieldType;
-            fn select<'a>(&self, store: &'a #name) -> &'a Self::FieldType;
-            fn set(&self, store: &mut #name, value: Self::FieldType);
-        }
-
-        #(#unit_structs)*
-    };
-
-    TokenStream::from(expanded)
+        Ok(CreteArgs { clone })
+    }
 }
 
-/// Generates code for atomic, thread-safe access to a struct via a global static store.
-///
-/// This macro produces:
-///
-/// - A static store (a `LazyLock` holding an `RwLock` protecting an `Arc` of the struct)
-///   whose identifier is based on the struct name (e.g. `CRETE_FOO` for a struct named `Foo`).
-///
-/// - An implementation of several associated methods on the struct:
-///     - `new()`: constructs a new instance using the struct's `Default` implementation.
-///     - `read()`: returns an `Arc`-wrapped shared reference to the current state.
-///     - `clone()`: (if the struct implements `Clone`) returns a cloned instance of the stored value.
-///     - `write(self)`: atomically replaces the current stored value with the provided one.
-///     - `select_ref<F: Field>(&self, field: F) -> &F::FieldType`: returns a reference to the selected field.
-///     - `get<F, R>(field: F, f: impl FnOnce(&F::FieldType) -> R) -> R`: applies a closure to a shared reference
-///       of the selected field.
-///     - `select<F: Field>(field: F) -> F::FieldType`: returns a cloned value of the selected field.
-///     - `set<F>(field: F, value: F::FieldType)`: updates a specific field and writes the new state to the store.
-///     - `update(f: impl FnOnce(&mut Self))`: applies a mutation closure to the current state and updates the store.
-///     - `update_async(f: impl AsyncFnOnce(&mut Self))`: an asynchronous version of `update` for non-blocking mutations.
-///
-/// The generated code leverages `std::sync::LazyLock`, `RwLock`, and `Arc` to ensure that all operations
-/// are safe to use concurrently from multiple threads.
-#[proc_macro_derive(Crete)]
-pub fn all_together_derive(input: TokenStream) -> TokenStream {
-    let input_clone = input.clone();
+#[proc_macro_attribute]
+pub fn crete(attr: TokenStream, item: TokenStream) -> TokenStream {
+    // Parse attribute parameters using our custom parser.
+    let args = parse_macro_input!(attr as CreteArgs);
 
-    // Parse the input tokens into a syntax tree
-    let input = parse_macro_input!(input as DeriveInput);
-
-    // Get the name of the struct
+    // Parse the struct definition.
+    let input = parse_macro_input!(item as DeriveInput);
     let struct_name = &input.ident;
 
-    // Generate the code from field_enum_derive
-    let field_enum_tokens: TokenStream2 = field_enum_derive(input_clone).into();
-
-    // Create a static identifier based on the struct name
-    let crete_store_ident = format_ident!("CRETE_{}", struct_name.to_string().to_uppercase());
-
-    // Check if the struct implements the `Clone` trait
-    let implements_clone = input.attrs.iter().any(|attr| {
+    // Check if the struct derives Clone.
+    let derives_clone = input.attrs.iter().any(|attr| {
         if attr.path().is_ident("derive") {
             let mut found_clone = false;
             let _ = attr.parse_nested_meta(|nested| {
                 if nested.path.is_ident("Clone") {
                     found_clone = true;
                 }
-                // Don't fail on non-`Clone` traits; keep parsing
                 Ok(())
             });
             found_clone
@@ -151,98 +82,206 @@ pub fn all_together_derive(input: TokenStream) -> TokenStream {
             false
         }
     });
-    dbg!(&implements_clone);
 
-    // Conditionally generate the `clone` function
-    let clone_fn = if implements_clone {
+    // * If the user types #[crete()] BEFORE #[derive(Clone)], it is implied -> #[crete(Clone)].
+    // * Allows the user to force true if the struct doesn't derive it but does impl manually.
+    let struct_is_clone = args.clone || derives_clone;
+    // println!("Struct name: {}, Struct is clone: {}, args.clone: {}, impl Clone: {}", struct_name, struct_is_clone, args.clone, derives_clone);
+
+    // Extract the fields (only works with named fields).
+    let fields = if let Data::Struct(DataStruct {
+                                         fields: Fields::Named(ref fields_named),
+                                         ..
+                                     }) = input.data
+    {
+        &fields_named.named
+    } else {
+        panic!("Crete can only be used with named fields");
+    };
+
+    // Define the Field trait. Note the store parameter is of type `#struct_name` (e.g. Store)
+    let field_trait = quote! {
+        pub trait Field {
+            type FieldType;
+            fn select<'a>(&self, store: &'a #struct_name) -> &'a Self::FieldType;
+            fn set(&self, store: &mut #struct_name, value: Self::FieldType);
+        }
+    };
+
+    // Generate unit structs and their implementations for each field.
+    let unit_structs = fields.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        let unit_struct_name = format_ident!("{}Field", ident.to_string().to_case(Case::Pascal));
+        let ty = &field.ty;
         quote! {
-            pub fn clone() -> #struct_name
-            {
-                let mut a = #crete_store_ident.read().expect("RWLock poisoned").clone();
-                ::std::sync::Arc::<#struct_name>::make_mut(&mut a).clone()
+            pub struct #unit_struct_name;
+
+            impl Field for #unit_struct_name {
+                type FieldType = #ty;
+                fn select<'a>(&self, store: &'a #struct_name) -> &'a Self::FieldType {
+                    &store.#ident
+                }
+                fn set(&self, store: &mut #struct_name, value: Self::FieldType) {
+                    store.#ident = value;
+                }
+            }
+        }
+    });
+
+    // Create the static store identifier.
+    let crete_store_ident = format_ident!("CRETE_{}", struct_name.to_string().to_uppercase());
+
+    let impl_block = if struct_is_clone {
+        quote! {
+            impl #struct_name {
+                pub fn new() -> Self {
+                    #struct_name::default()
+                }
+
+                pub fn read() -> Arc<#struct_name> {
+                    #crete_store_ident.read().expect("RWLock poisoned").clone()
+                }
+
+                pub fn clone() -> #struct_name {
+                    let mut a = #crete_store_ident.read().expect("RWLock poisoned").clone();
+                    Arc::make_mut(&mut a).clone()
+                }
+
+                pub fn write(self) {
+                    *#crete_store_ident.write().expect("RWLock poisoned") = Arc::new(self);
+                }
+
+                pub fn select_ref<F: Field>(&self, field: F) -> &F::FieldType {
+                    field.select(self)
+                }
+
+                pub fn get<F, R>(field: F, f: impl FnOnce(&F::FieldType) -> R) -> R
+                where
+                    F: Field,
+                {
+                    let store = #struct_name::read();
+                    let field_ref = store.select_ref(field);
+                    f(field_ref)
+                }
+
+                pub fn select<F: Field>(field: F) -> F::FieldType
+                where
+                    F::FieldType: Clone,
+                {
+                    #struct_name::read().select_ref(field).clone()
+                }
+
+                pub fn set<F>(field: F, value: F::FieldType)
+                where
+                    F: Field,
+                {
+                    let mut store_write_guard = #crete_store_ident.write().expect("RWLock poisoned");
+                    let mut s = Arc::make_mut(&mut *store_write_guard);
+
+                    field.set(&mut s, value);
+                }
+
+                pub fn update(f: impl FnOnce(&mut #struct_name) -> ()) {
+                    let mut store_write_guard = #crete_store_ident.write().expect("RWLock poisoned");
+                    let s = Arc::make_mut(&mut *store_write_guard);
+
+                    f(s);
+                }
+
+                pub async fn update_async<F>(f: F)
+                where
+                    F: AsyncFnOnce(&mut #struct_name),
+                {
+                    let mut store_write_guard = #crete_store_ident.write().expect("RWLock poisoned");
+                    let s = Arc::make_mut(&mut *store_write_guard);
+
+                    f(s).await;
+                }
             }
         }
     } else {
-        quote! {} // Do not generate the function if Clone is not implemented
-    };
+        quote! {
+            impl #struct_name {
+                pub fn new() -> Self {
+                    #struct_name::default()
+                }
 
-    // Generate the necessary code
-    let code = quote! {
-        #field_enum_tokens
+                pub fn write(self) {
+                    let store_arc = #crete_store_ident.clone();
+                    let mut store_guard = store_arc.write().expect("RWLock poisoned");
+                    *store_guard = self;
+                }
 
-        static #crete_store_ident: ::std::sync::LazyLock<::std::sync::RwLock<::std::sync::Arc<#struct_name>>> =
-            ::std::sync::LazyLock::new(|| ::std::sync::RwLock::new(::std::sync::Arc::new(#struct_name::new())));
+                pub fn select_ref<F: Field>(&self, field: F) -> &F::FieldType {
+                    field.select(self)
+                }
 
-        impl #struct_name {
-            pub fn new() -> Self {
-                #struct_name::default()
-            }
+                pub fn get<F, R>(field: F, f: impl FnOnce(&F::FieldType) -> R) -> R
+                where
+                    F: Field,
+                {
+                    let store_arc = #crete_store_ident.clone();
+                    let store_guard = store_arc.read().expect("RWLock poisoned");
+                    let field_ref = field.select(&*store_guard);
 
-            pub fn read() -> ::std::sync::Arc<#struct_name> {
-                #crete_store_ident.read().expect("RWLock poisoned").clone()
-            }
+                    f(field_ref)
+                }
 
-            #clone_fn
+                pub fn set<F>(field: F, value: F::FieldType)
+                where
+                    F: Field,
+                {
+                    let store_arc = #crete_store_ident.clone();
+                    let mut store_guard = store_arc.write().expect("RWLock poisoned");
 
-            pub fn write(self) {
-                *#crete_store_ident.write().expect("RWLock poisoned") = ::std::sync::Arc::new(self);
-            }
+                    field.set(&mut *store_guard, value);
+                }
 
-            pub fn select_ref<F: Field>(&self, field: F) -> &F::FieldType {
-                field.select(self)
-            }
+                pub fn update(f: impl FnOnce(&mut #struct_name) -> ()) {
+                    let store_arc = #crete_store_ident.clone();
+                    let mut store_guard = store_arc.write().expect("RWLock poisoned");
 
-            pub fn get<F, R>(field: F, f: impl FnOnce(&F::FieldType) -> R) -> R
-            where
-                F: Field,
-            {
-                let store = #struct_name::read();
-                let field_ref = store.select_ref(field);
-                f(field_ref)
-            }
+                    f(&mut *store_guard);
+                }
 
-            pub fn select<F: Field>(field: F) -> F::FieldType
-            where
-                F::FieldType: Clone,
-            {
-                #struct_name::read().select_ref(field).clone()
-            }
+                pub async fn update_async<F>(f: F)
+                where
+                    F: AsyncFnOnce(&mut #struct_name),
+                {
+                    let store_arc = #crete_store_ident.clone();
+                    let mut store_guard = store_arc.write().expect("RWLock poisoned");
 
-            pub fn set<F>(field: F, value: F::FieldType)
-            where
-                F: Field,
-            {
-                let mut store_write_guard = #crete_store_ident.write().expect("RWLock poisoned");
-                let mut s = ::std::sync::Arc::get_mut(&mut *store_write_guard).expect("You have an extra strong reference. Did you have a read() binding in the same scope?");
-
-                field.set(&mut s, value);
-
-                *store_write_guard = ::std::sync::Arc::new(::std::mem::take(s));
-            }
-
-            pub fn update(f: impl FnOnce(&mut #struct_name) -> ()) {
-                let mut store_write_guard = #crete_store_ident.write().expect("RWLock poisoned");
-                let mut s = ::std::sync::Arc::get_mut(&mut *store_write_guard).expect("You have an extra strong reference. Did you have a read() binding in the same scope?");
-
-
-                f(&mut s);
-
-                *store_write_guard = ::std::sync::Arc::new(::std::mem::take(s));
-            }
-
-            pub async fn update_async<F>(f: F)
-            where
-                F: AsyncFnOnce(&mut #struct_name),
-            {
-                let mut store_write_guard = #crete_store_ident.write().expect("RWLock poisoned");
-                let mut s = ::std::sync::Arc::get_mut(&mut *store_write_guard).expect("You have an extra strong reference. Did you have a read() binding in the same scope?");
-
-                f(&mut s).await;
-
-                *store_write_guard = ::std::sync::Arc::new(::std::mem::take(s));
+                    f(&mut *store_guard).await;
+                }
             }
         }
     };
 
-    code.into()
+    let static_store = if struct_is_clone {
+        quote! {
+            static #crete_store_ident: LazyLock<RwLock<Arc<#struct_name>>> =
+                LazyLock::new(|| RwLock::new(Arc::new(#struct_name::new())));
+        }
+    } else {
+        quote! {
+            static #crete_store_ident: LazyLock<Arc<RwLock<#struct_name>>> =
+                LazyLock::new(|| Arc::new(RwLock::new(#struct_name::new())));
+        }
+    };
+
+    let expanded = quote! {
+        use std::sync::{Arc, RwLock, LazyLock};
+
+        #input
+
+        #field_trait
+
+        #(#unit_structs)*
+
+        #static_store
+
+        #impl_block
+    };
+
+    TokenStream::from(expanded)
 }
